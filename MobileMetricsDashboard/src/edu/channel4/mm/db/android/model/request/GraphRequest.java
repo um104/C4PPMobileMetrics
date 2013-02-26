4bd5fc038bb0fa2a9e6d488ca1fa22c44fb19620
Mark Lerner <g.um104.w@gmail.com>
2013-02-25 18:52:57 -0800

    Change GraphRequest interface to have URLParamString method, and got rid of the Map methods.

--- MobileMetricsDashboard/src/edu/channel4/mm/db/android/model/request/GraphRequest.java
+++ MobileMetricsDashboard/src/edu/channel4/mm/db/android/model/request/GraphRequest.java
@@ -2,7 +2,6 @@ package edu.channel4.mm.db.android.model.request;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import android.content.Context;
 import android.content.Intent;
@@ -11,70 +10,63 @@ import edu.channel4.mm.db.android.activity.GraphViewerActivity;
 
 public interface GraphRequest {
 
-	@Override
-	public String toString();
+   @Override
+   public String toString();
 
-	/**
-	 * Read-only getter for the REST request type that the server expects.
-	 * 
-	 * @return
-	 */
-	public String getRestRequestType();
+   /**
+    * Read-only getter for the REST request type that the server expects.
+    * 
+    * @return
+    */
+   public String getRestRequestType();
 
-	/**
-	 * Read-only getter for the fields in this {@link GraphRequest}
-	 */
-	public Map<String, String> getFields();
+   /**
+    * Use URLEncodedUtils.format(params, "utf-8") to generate this return string
+    * 
+    * @return The string representation of the parameters held in this
+    *         GraphRequest.
+    */
+   public String getUrlParameterString();
 
-	/**
-	 * Write-only setter used to add a new field to this {@link GraphRequest}
-	 * 
-	 * @param key
-	 * @param value
-	 * @return the value of any previous mapping with the specified key or null
-	 *         if there was no mapping.
-	 */
-	public String addField(String key, String value);
+   /**
+    * Constructs the correct Intent for a given GraphRequest. The Intent will
+    * open the correct next Activity (e.g. Number of Sessions over Time goes
+    * straight to {@link GraphViewerActivity} whereas Events over Time requires
+    * you to choose the Event first in {@link EventPickerActivity}.
+    * 
+    * The Intent will add fields to the GraphRequest as it is passed along the
+    * Activities.
+    * 
+    * @param context
+    * @param graphRequest
+    * @return
+    */
+   public Intent constructGraphRequestIntent(Context context);
 
-	/**
-	 * Constructs the correct Intent for a given GraphRequest. The Intent will
-	 * open the correct next Activity (e.g. Number of Sessions over Time goes
-	 * straight to {@link GraphViewerActivity} whereas Events over Time requires
-	 * you to choose the Event first in {@link EventPickerActivity}.
-	 * 
-	 * The Intent will add fields to the GraphRequest as it is passed along the
-	 * Activities.
-	 * 
-	 * @param context
-	 * @param graphRequest
-	 * @return
-	 */
-	public Intent constructGraphRequestIntent(Context context);
+   public enum StartTime {
+      LAST_24_HOURS("From the last 24 hours"), LAST_WEEK("From the last week"), LAST_MONTH(
+               "From the last month");
 
-	public enum StartTime {
-		LAST_24_HOURS("From the last 24 hours"), LAST_WEEK("From the last week"), LAST_MONTH(
-				"From the last month");
+      private String displayName;
 
-		private String displayName;
+      private StartTime(String displayName) {
+         this.displayName = displayName;
+      }
 
-		private StartTime(String displayName) {
-			this.displayName = displayName;
-		}
+      @Override
+      public String toString() {
+         return displayName;
+      }
 
-		@Override
-		public String toString() {
-			return displayName;
-		}
+      public static List<String> getStringArray() {
+         List<String> startTimes = new ArrayList<String>(
+                  GraphRequest.StartTime.values().length);
+         for (GraphRequest.StartTime startTime : GraphRequest.StartTime
+                  .values()) {
+            startTimes.add(startTime.toString());
+         }
 
-		public static List<String> getStringArray() {
-			List<String> startTimes = new ArrayList<String>(
-					GraphRequest.StartTime.values().length);
-			for (GraphRequest.StartTime startTime : GraphRequest.StartTime
-					.values()) {
-				startTimes.add(startTime.toString());
-			}
-
-			return startTimes;
-		}
-	}
+         return startTimes;
+      }
+   }
 }
