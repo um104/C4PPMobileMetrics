3646a1822730815b807f63949f778e6b27f4f628
Mark Lerner <g.um104.w@gmail.com>
2013-03-07 18:43:33 -0800

    Added new .png's for Usage and Audience icons. Changed Cell layout xmls to all have black text, and be layed out better. Changed DashboardActivity to use a ListView and formatting.

--- MobileMetricsDashboard/src/edu/channel4/mm/db/android/activity/DashboardActivity.java
+++ MobileMetricsDashboard/src/edu/channel4/mm/db/android/activity/DashboardActivity.java
@@ -23,159 +23,171 @@ import edu.channel4.mm.db.android.util.Keys;
 
 public class DashboardActivity extends Activity {
 
-	private GridView gridView;
-	private GridViewAdapter adapter;
-	private TextView appLabelView;
-	
-	private ListView listView;
+   // private GridView gridView;
+   private GridViewAdapter adapter;
+   private TextView appLabelView;
 
-	@SuppressLint("NewApi")
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_dashboard);
-		
-		// Setup the ListView
-		listView = (ListView) findViewById(R.id.listViewDashboard);
+   private ListView listView;
 
-		// Setup the GridView
-		gridView = (GridView) findViewById(R.id.gridViewDashboard);
-		adapter = new GridViewAdapter();
-		gridView.setAdapter(adapter);
-		gridView.setOnItemClickListener(new OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> parent, View v,
-					int position, long id) {
-				startActivity(new Intent(getApplicationContext(),
-						 adapter.subactivityDetails[position].activityClass));
-			}
-		});
+   // ExpandableListView;
 
-		// Make sure we're running on Honeycomb or higher to use ActionBar APIs
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-			// Show the Up button in the action bar.
-			getActionBar().setDisplayHomeAsUpEnabled(true);
-		}
+   @SuppressLint("NewApi")
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_dashboard);
 
-		String appLabel = getApplicationContext().getSharedPreferences(Keys.PREFS_NS, 0).getString(Keys.APP_LABEL, null);
+      // Setup the ListView
+      listView = (ListView) findViewById(R.id.listViewDashboard);
+      adapter = new GridViewAdapter();
+      listView.setAdapter(adapter);
+      listView.setOnItemClickListener(new OnItemClickListener(){
+         @Override
+         public void onItemClick(AdapterView<?> parent, View v, int position,
+                  long id) {
+            startActivity(new Intent(getApplicationContext(),
+                     adapter.subactivityDetails[position].activityClass));
+         }
+      });
 
-		appLabelView = (TextView) findViewById(R.id.textViewDashboardTitle);
-		appLabelView.setText(appLabel);
-	}
+      // Make sure we're running on Honeycomb or higher to use ActionBar APIs
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+         // Show the Up button in the action bar.
+         getActionBar().setDisplayHomeAsUpEnabled(true);
+      }
 
-	@Override
-	protected void onResume() {
-		super.onResume();
+      String appLabel = getApplicationContext().getSharedPreferences(
+               Keys.PREFS_NS, 0).getString(Keys.APP_LABEL, null);
 
-		// TODO(mlerner): Make call to APEX to get favorite graphs, recent
-		// graphs, other info to display.
-	}
+      appLabelView = (TextView) findViewById(R.id.textViewDashboardTitle);
+      appLabelView.setText(appLabel);
+   }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_dashboard, menu);
-		return true;
-	}
+   @Override
+   protected void onResume() {
+      super.onResume();
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			// This ID represents the Home or Up button. In the case of this
-			// activity, the Up button is shown. Use NavUtils to allow users
-			// to navigate up one level in the application structure. For
-			// more details, see the Navigation pattern on Android Design:
-			//
-			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
-			//
-			NavUtils.navigateUpFromSameTask(this);
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+      // TODO(mlerner): Make call to APEX to get favorite graphs, recent
+      // graphs, other info to display.
+   }
 
-	/**
-	 * Helper class that aggregates details on the Activities you want to enter.
-	 * These are presented to the user by the GridViewAdapter
-	 */
-	static class SubactivityDetails {
-		private Integer iconID;
-		private String displayName;
-		private Class<?> activityClass;
+   @Override
+   public boolean onCreateOptionsMenu(Menu menu) {
+      // Inflate the menu; this adds items to the action bar if it is present.
+      getMenuInflater().inflate(R.menu.activity_dashboard, menu);
+      return true;
+   }
 
-		public SubactivityDetails(Integer iconID, String displayName,
-				Class<?> activityClass) {
-			this.iconID = iconID;
-			this.displayName = displayName;
-			this.activityClass = activityClass;
-		}
-	}
+   @Override
+   public boolean onOptionsItemSelected(MenuItem item) {
+      switch (item.getItemId()) {
+         case android.R.id.home:
+            // This ID represents the Home or Up button. In the case of this
+            // activity, the Up button is shown. Use NavUtils to allow users
+            // to navigate up one level in the application structure. For
+            // more details, see the Navigation pattern on Android Design:
+            //
+            // http://developer.android.com/design/patterns/navigation.html#up-vs-back
+            //
+            NavUtils.navigateUpFromSameTask(this);
+            return true;
+      }
+      return super.onOptionsItemSelected(item);
+   }
 
-	/**
-	 * Helper adapter for the GridView.
-	 */
-	class GridViewAdapter extends BaseAdapter {
-		protected final SubactivityDetails[] subactivityDetails = new SubactivityDetails[] {
-				new SubactivityDetails(R.drawable.ic_launcher, getResources()
-						.getString(R.string.usage), UsageActivity.class),
-				new SubactivityDetails(R.drawable.ic_launcher, getResources()
-						.getString(R.string.audience), AudienceActivity.class),
-				new SubactivityDetails(R.drawable.ic_launcher, getResources()
-						.getString(R.string.event_funnels),
-						EventFunnelsActivity.class),
-				new SubactivityDetails(R.drawable.ic_launcher, getResources()
-						.getString(R.string.custom_graphs),
-						CustomGraphActivity.class) };
+   /**
+    * Helper class that aggregates details on the Activities you want to enter.
+    * These are presented to the user by the GridViewAdapter
+    */
+   static class SubactivityDetails {
+      private Integer iconID;
+      private String displayName;
+      private Class<?> activityClass;
 
-		public GridViewAdapter() {
-		}
+      public SubactivityDetails(Integer iconID, String displayName,
+                                Class<?> activityClass) {
+         this.iconID = iconID;
+         this.displayName = displayName;
+         this.activityClass = activityClass;
+      }
+   }
 
-		public int getCount() {
-			return subactivityDetails.length;
-		}
+   /**
+    * Helper adapter for the GridView.
+    */
+   class GridViewAdapter extends BaseAdapter {
+      protected final SubactivityDetails[] subactivityDetails = new SubactivityDetails[] {
+                                                                                          new SubactivityDetails(
+                                                                                                   R.drawable.usage_icon,
+                                                                                                   getResources()
+                                                                                                            .getString(
+                                                                                                                     R.string.usage),
+                                                                                                   UsageActivity.class),
+                                                                                          new SubactivityDetails(
+                                                                                                   R.drawable.audience_icon,
+                                                                                                   getResources()
+                                                                                                            .getString(
+                                                                                                                     R.string.audience),
+                                                                                                   AudienceActivity.class),
+      /*
+       * new SubactivityDetails(R.drawable.ic_launcher, getResources()
+       * .getString(R.string.event_funnels), EventFunnelsActivity.class),
+       */
+      /*
+       * new SubactivityDetails(R.drawable.ic_launcher, getResources()
+       * .getString(R.string.custom_graphs), CustomGraphActivity.class)
+       */
+      };
 
-		public Object getItem(int position) {
-			return null;
-		}
+      public GridViewAdapter() {
+      }
 
-		public long getItemId(int position) {
-			return 0;
-		}
+      public int getCount() {
+         return subactivityDetails.length;
+      }
 
-		// create a new ImageView for each item referenced by the Adapter
-		public View getView(int position, View convertView, ViewGroup parent) {
-			View gridViewCell;
-			ImageView icon;
-			if (convertView == null) { // if it's not recycled, initialize some
-										// attributes
-				LayoutInflater inflater = getLayoutInflater();
-				gridViewCell = inflater.inflate(
-						R.layout.cell_dashboard_gridview_piece, parent, false);
+      public Object getItem(int position) {
+         return null;
+      }
 
-				// gridViewCell = new ImageView(getApplicationContext());
-				icon = (ImageView) gridViewCell
-						.findViewById(R.id.imageViewDashboardCellIcon);
+      public long getItemId(int position) {
+         return 0;
+      }
 
-				// icon.setLayoutParams(new GridView.LayoutParams(85, 85));
-				icon.setScaleType(ImageView.ScaleType.CENTER_CROP);
-				icon.setPadding(8, 8, 8, 8);
-			} else {
-				gridViewCell = (View) convertView;
-				icon = (ImageView) gridViewCell
-						.findViewById(R.id.imageViewDashboardCellIcon);
-			}
+      // create a new ImageView for each item referenced by the Adapter
+      public View getView(int position, View convertView, ViewGroup parent) {
+         View gridViewCell;
+         ImageView icon;
+         if (convertView == null) { // if it's not recycled, initialize some
+            // attributes
+            LayoutInflater inflater = getLayoutInflater();
+            gridViewCell = inflater.inflate(
+                     R.layout.cell_dashboard_gridview_piece, parent, false);
 
-			// Set the icon to be the correct icon
-			icon.setImageResource(subactivityDetails[position].iconID);
+            // gridViewCell = new ImageView(getApplicationContext());
+            icon = (ImageView) gridViewCell
+                     .findViewById(R.id.imageViewDashboardCellIcon);
 
-			// Set the label to have the right text
-			TextView label = (TextView) gridViewCell
-					.findViewById(R.id.textViewDashboardCellLabel);
-			label.setText(subactivityDetails[position].displayName);
+            // icon.setLayoutParams(new GridView.LayoutParams(85, 85));
+            icon.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            icon.setPadding(8, 8, 8, 8);
+         }
+         else {
+            gridViewCell = (View) convertView;
+            icon = (ImageView) gridViewCell
+                     .findViewById(R.id.imageViewDashboardCellIcon);
+         }
 
-			return gridViewCell;
-		}
-	}
+         // Set the icon to be the correct icon
+         icon.setImageResource(subactivityDetails[position].iconID);
+
+         // Set the label to have the right text
+         TextView label = (TextView) gridViewCell
+                  .findViewById(R.id.textViewDashboardCellLabel);
+         label.setText(subactivityDetails[position].displayName);
+
+         return gridViewCell;
+      }
+   }
 
 }
