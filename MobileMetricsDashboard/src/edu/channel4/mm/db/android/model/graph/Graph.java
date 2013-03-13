3122a24f5ff9e5f1fb81a2ba22f963f820edcbb8
Brian Middaugh <bmiddaug@calpoly.edu>
2013-03-13 02:10:27 -0700

    beautiful bar charts

--- MobileMetricsDashboard/src/edu/channel4/mm/db/android/model/graph/Graph.java
+++ MobileMetricsDashboard/src/edu/channel4/mm/db/android/model/graph/Graph.java
@@ -1,5 +1,7 @@
 package edu.channel4.mm.db.android.model.graph;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 
@@ -11,10 +13,12 @@ import org.achartengine.model.XYSeries;
 import org.achartengine.renderer.DefaultRenderer;
 import org.achartengine.renderer.SimpleSeriesRenderer;
 import org.achartengine.renderer.XYMultipleSeriesRenderer;
+import org.achartengine.renderer.XYMultipleSeriesRenderer.Orientation;
 
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.Paint.Align;
 
 import com.google.visualization.datasource.datatable.DataTable;
 import com.google.visualization.datasource.datatable.TableRow;
@@ -25,160 +29,271 @@ import edu.channel4.mm.db.android.util.Log;
 
 public class Graph {
 
-   private String title;
-   private List<GraphType> validGraphTypes;
-   private DataTable datatable;
+	private String title;
+	private List<GraphType> validGraphTypes;
+	private DataTable datatable;
 
-   public Graph(String title, List<GraphType> validGraphTypes,
-                DataTable datatable) {
-      this.title = title;
-      this.validGraphTypes = validGraphTypes;
-      this.datatable = datatable;
-   }
+	public Graph(String title, List<GraphType> validGraphTypes,
+			DataTable datatable) {
+		this.title = title;
+		this.validGraphTypes = validGraphTypes;
+		this.datatable = datatable;
+	}
 
-   public String getTitle() {
-      return title;
-   }
+	public String getTitle() {
+		return title;
+	}
 
-   /**
-    * Gets a random Intent for ACE to draw.
-    * 
-    * @param context
-    * @return
-    */
-   public Intent getRandomIntent(Context context) {
+	/**
+	 * Gets a random Intent for ACE to draw.
+	 * 
+	 * @param context
+	 * @return
+	 */
+	public Intent getRandomIntent(Context context) {
 
-      // Pick a random number in the validGraphTypes
-      Random rand = new Random();
-      int element = rand.nextInt(validGraphTypes.size());
+		// Pick a random number in the validGraphTypes
+		Random rand = new Random();
+		int element = rand.nextInt(validGraphTypes.size());
 
-      GraphType graphType = validGraphTypes.get(element);
+		GraphType graphType = validGraphTypes.get(element);
 
-      switch (graphType) {
-         case PIE:
-            return getPieGraphIntent(context);
-         case BAR:
-            return getBarGraphIntent(context);
-         case LINE:
-            return getLineGraphIntent(context);
-         default:
-            Log.e("Unknown graphType: " + graphType.toString());
-            return null;
-      }
-   }
+		switch (graphType) {
+		case PIE:
+			return getPieGraphIntent(context);
+		case BAR:
+			return getBarGraphIntent(context);
+		case LINE:
+			return getLineGraphIntent(context);
+		default:
+			Log.e("Unknown graphType: " + graphType.toString());
+			return null;
+		}
+	}
 
-   private Intent getPieGraphIntent(Context context) {
-      Intent intent = null;
-      Random rand = new Random();
+	private Intent getPieGraphIntent(Context context) {
+		Intent intent = null;
+		Random rand = new Random();
 
-      CategorySeries categorySeries = new CategorySeries("unknown pie chart");
-      DefaultRenderer renderer = new DefaultRenderer();
+		CategorySeries categorySeries = new CategorySeries("unknown pie chart");
+		DefaultRenderer renderer = new DefaultRenderer();
 
-      for (TableRow tableRow : datatable.getRows()) {
-         // FIXME: Hack that assumes that column 0 is string and column 1 is
-         // number
-         String category = ((TextValue) tableRow.getCell(0).getValue())
-                  .getValue();
-         Double value = ((NumberValue) tableRow.getCell(1).getValue())
-                  .getValue();
-         categorySeries.add(category, value);
-         SimpleSeriesRenderer simpleSeriesRenderer = new SimpleSeriesRenderer();
-         simpleSeriesRenderer.setColor(Color.rgb(rand.nextInt(255), rand.nextInt(255), rand.nextInt(255)));
-         renderer.addSeriesRenderer(simpleSeriesRenderer);
-      }
+		for (TableRow tableRow : datatable.getRows()) {
+			// FIXME: Hack that assumes that column 0 is string and column 1 is
+			// number
+			String category = ((TextValue) tableRow.getCell(0).getValue())
+					.getValue();
+			Double value = ((NumberValue) tableRow.getCell(1).getValue())
+					.getValue();
+			categorySeries.add(category, value);
+			SimpleSeriesRenderer simpleSeriesRenderer = new SimpleSeriesRenderer();
+			simpleSeriesRenderer.setColor(Color.rgb(rand.nextInt(255),
+					rand.nextInt(255), rand.nextInt(255)));
+			renderer.addSeriesRenderer(simpleSeriesRenderer);
+		}
 
-      /*SimpleSeriesRenderer simpleSeriesRenderer = new SimpleSeriesRenderer();
-      renderer.addSeriesRenderer(simpleSeriesRenderer);*/
+		/*
+		 * SimpleSeriesRenderer simpleSeriesRenderer = new
+		 * SimpleSeriesRenderer();
+		 * renderer.addSeriesRenderer(simpleSeriesRenderer);
+		 */
 
-      intent = ChartFactory.getPieChartIntent(context, categorySeries,
-               renderer, "some activity");
+		intent = ChartFactory.getPieChartIntent(context, categorySeries,
+				renderer, "some activity");
 
-      return intent;
-   }
+		return intent;
+	}
 
-   private Intent getBarGraphIntent(Context context) {
-      return setupXYMultipleSeriesIntent(getBarRenderer(), context);
-   }
+	private Intent getBarGraphIntent(Context context) {
+		String[] titles = new String[] { "Count"};
+        List<double[]> values = new ArrayList<double[]>();
+        List<String> categories = new ArrayList<String>();
+        
+        int xmax = 0;
+        double ymax = 0;
+        
+        double[] firstVals = new double[20];
+        
+		for (TableRow tableRow : datatable.getRows()) {
+			// FIXME: Hack that assumes that column 0 is string and column 1 is
+			// number
+			String category = ((TextValue) tableRow.getCell(0).getValue())
+					.getValue();
+			Double value = ((NumberValue) tableRow.getCell(1).getValue())
+					.getValue();
+			if(value > ymax){
+				ymax = value;
+			}
+			
+			firstVals[xmax] = value;
+			categories.add(category);
+			xmax++;
+		}
+		
+		values.add(firstVals);
+		
+        int[] colors = new int[] { Color.parseColor("#77c4d3")};
+        XYMultipleSeriesRenderer renderer = buildBarRenderer(colors);
+        renderer.setOrientation(Orientation.HORIZONTAL);
+        setChartSettings(renderer, "Production Details", "Products", "Count", 0,
+            xmax + 1, 0, 1.3 * ymax, Color.BLACK, Color.BLACK);
+        
+        renderer.setXLabels(1);
+        renderer.setYLabels(10);
+        
+        for(int i = 1; i < xmax; i++){
+        	renderer.addXTextLabel(i, categories.get(i));
+        }
+        
+        int length = renderer.getSeriesRendererCount();
+        for (int i = 0; i < length; i++) {
+          SimpleSeriesRenderer seriesRenderer = renderer.getSeriesRendererAt(i);
+          seriesRenderer.setDisplayChartValues(true);
+        }
+ 
+ 
+        return ChartFactory.getBarChartIntent(context, buildBarDataset(titles, values), renderer,Type.DEFAULT);
+   
+		//return setupXYMultipleSeriesIntent(getBarRenderer(), context);
+	}
 
-   private Intent getLineGraphIntent(Context context) {
-      return setupXYMultipleSeriesIntent(getLineRenderer(), context);
-   }
+	private Intent getLineGraphIntent(Context context) {
+		return setupXYMultipleSeriesIntent(getLineRenderer(), context);
+	}
 
-   private Intent setupXYMultipleSeriesIntent(
-            XYMultipleSeriesRenderer renderer, Context context) {
-      Intent intent = null;
+	private Intent setupXYMultipleSeriesIntent(
+			XYMultipleSeriesRenderer renderer, Context context) {
+		Intent intent = null;
 
-      XYMultipleSeriesDataset dataset = new XYMultipleSeriesDataset();
-      XYSeries xySeries = new XYSeries("some xy series");
+		XYMultipleSeriesDataset dataset = new XYMultipleSeriesDataset();
+		XYSeries xySeries = new XYSeries("some xy series");
 
-      int i = 1;
-      for (TableRow tableRow : datatable.getRows()) {
+		int i = 1;
+		for (TableRow tableRow : datatable.getRows()) {
 
-         // FIXME: Hack that assumes that column 0 is string and column 1 is
-         // number
-         String category = ((TextValue) tableRow.getCell(0).getValue())
-                  .getValue();
-         Double value = ((NumberValue) tableRow.getCell(1).getValue())
-                  .getValue();
-         xySeries.add(i, value);
-         renderer.addXTextLabel(i, category);
-         i++;
-      }
+			// FIXME: Hack that assumes that column 0 is string and column 1 is
+			// number
+			String category = ((TextValue) tableRow.getCell(0).getValue())
+					.getValue();
+			Double value = ((NumberValue) tableRow.getCell(1).getValue())
+					.getValue();
+			xySeries.add(i, value);
+			renderer.addXTextLabel(i, category);
+			i++;
+		}
 
-      dataset.addSeries(xySeries);
+		dataset.addSeries(xySeries);
 
-      intent = ChartFactory.getBarChartIntent(context, dataset, renderer,
-               Type.DEFAULT);
+		intent = ChartFactory.getBarChartIntent(context, dataset, renderer,
+				Type.DEFAULT);
 
-      return intent;
-   }
+		return intent;
+	}
 
-   private static XYMultipleSeriesRenderer getBarRenderer() {
-      // Define a renderer
-      XYMultipleSeriesRenderer renderer = new XYMultipleSeriesRenderer();
-      renderer.setAxisTitleTextSize(16);
-      renderer.setChartTitleTextSize(20);
-      renderer.setLabelsTextSize(15);
-      renderer.setLegendTextSize(15);
-      renderer.setMargins(new int[] {20, 30, 15, 0});
-      SimpleSeriesRenderer r = new SimpleSeriesRenderer();
-      r.setColor(Color.GREEN);
-      renderer.addSeriesRenderer(r);
+	private XYMultipleSeriesRenderer buildBarRenderer(int[] colors) {
+		XYMultipleSeriesRenderer renderer = new XYMultipleSeriesRenderer();
+		renderer.setAxisTitleTextSize(16);
+		renderer.setChartTitleTextSize(20);
+		renderer.setLabelsTextSize(15);
+		renderer.setLegendTextSize(15);
+		renderer.setBarSpacing(1);
 
-      // Set chart settings
-      renderer.setChartTitle("Chart demo");
-      renderer.setXTitle("x values");
-      renderer.setYTitle("y values");
-      renderer.setXAxisMin(0.5);
-      renderer.setXAxisMax(10.5);
-      renderer.setYAxisMin(0);
-      renderer.setYAxisMax(210);
+		renderer.setMarginsColor(Color.parseColor("#EEEDED"));
+		renderer.setXLabelsColor(Color.BLACK);
+		renderer.setYLabelsColor(0, Color.BLACK);
 
-      return renderer;
-   }
+		renderer.setApplyBackgroundColor(true);
+		renderer.setBackgroundColor(Color.parseColor("#FBFBFC"));
 
-   private static XYMultipleSeriesRenderer getLineRenderer() {
-      // Define a renderer
-      XYMultipleSeriesRenderer renderer = new XYMultipleSeriesRenderer();
-      renderer.setAxisTitleTextSize(16);
-      renderer.setChartTitleTextSize(20);
-      renderer.setLabelsTextSize(15);
-      renderer.setLegendTextSize(15);
-      renderer.setMargins(new int[] {20, 30, 15, 0});
-      SimpleSeriesRenderer r = new SimpleSeriesRenderer();
-      r.setColor(Color.GREEN);
-      renderer.addSeriesRenderer(r);
+		int length = colors.length;
+		for (int i = 0; i < length; i++) {
+			SimpleSeriesRenderer r = new SimpleSeriesRenderer();
+			r.setColor(colors[i]);
+			r.setChartValuesSpacing(15);
+			renderer.addSeriesRenderer(r);
+		}
+		return renderer;
+	}
 
-      // Set chart settings
-      renderer.setChartTitle("Chart demo");
-      renderer.setXTitle("x values");
-      renderer.setYTitle("y values");
-      renderer.setXAxisMin(0.5);
-      renderer.setXAxisMax(10.5);
-      renderer.setYAxisMin(0);
-      renderer.setYAxisMax(210);
+	private XYMultipleSeriesDataset buildBarDataset(String[] titles,
+			List<double[]> values) {
+		XYMultipleSeriesDataset dataset = new XYMultipleSeriesDataset();
+		int length = titles.length;
+		for (int i = 0; i < length; i++) {
+			CategorySeries series = new CategorySeries(titles[i]);
+			double[] v = values.get(i);
+			int seriesLength = v.length;
+			for (int k = 0; k < seriesLength; k++) {
+				series.add(v[k]);
+			}
+			dataset.addSeries(series.toXYSeries());
+		}
+		return dataset;
+	}
 
-      return renderer;
-   }
+	private void setChartSettings(XYMultipleSeriesRenderer renderer,
+			String title, String xTitle, String yTitle, double xMin,
+			double xMax, double yMin, double yMax, int axesColor,
+			int labelsColor) {
+		renderer.setChartTitle(title);
+		renderer.setYLabelsAlign(Align.RIGHT);
+		renderer.setXTitle(xTitle);
+		renderer.setYTitle(yTitle);
+		renderer.setXAxisMin(xMin);
+		renderer.setXAxisMax(xMax);
+		renderer.setYAxisMin(yMin);
+		renderer.setYAxisMax(yMax);
+		renderer.setMargins(new int[] { 10, 65, 10, 15 });
+		renderer.setAxesColor(axesColor);
+		renderer.setLabelsColor(labelsColor);
+	}
+
+	private static XYMultipleSeriesRenderer getBarRenderer() {
+		// Define a renderer
+		XYMultipleSeriesRenderer renderer = new XYMultipleSeriesRenderer();
+		renderer.setAxisTitleTextSize(16);
+		renderer.setChartTitleTextSize(20);
+		renderer.setLabelsTextSize(15);
+		renderer.setLegendTextSize(15);
+		renderer.setMargins(new int[] { 20, 30, 15, 0 });
+		SimpleSeriesRenderer r = new SimpleSeriesRenderer();
+		r.setColor(Color.GREEN);
+		renderer.addSeriesRenderer(r);
+
+		// Set chart settings
+		renderer.setChartTitle("Chart demo");
+		renderer.setXTitle("x values");
+		renderer.setYTitle("y values");
+		renderer.setXAxisMin(0.5);
+		renderer.setXAxisMax(10.5);
+		renderer.setYAxisMin(0);
+		renderer.setYAxisMax(210);
+
+		return renderer;
+	}
+
+	private static XYMultipleSeriesRenderer getLineRenderer() {
+		// Define a renderer
+		XYMultipleSeriesRenderer renderer = new XYMultipleSeriesRenderer();
+		renderer.setAxisTitleTextSize(16);
+		renderer.setChartTitleTextSize(20);
+		renderer.setLabelsTextSize(15);
+		renderer.setLegendTextSize(15);
+		renderer.setMargins(new int[] { 20, 30, 15, 0 });
+		SimpleSeriesRenderer r = new SimpleSeriesRenderer();
+		r.setColor(Color.GREEN);
+		renderer.addSeriesRenderer(r);
+
+		// Set chart settings
+		renderer.setChartTitle("Chart demo");
+		renderer.setXTitle("x values");
+		renderer.setYTitle("y values");
+		renderer.setXAxisMin(0.5);
+		renderer.setXAxisMax(10.5);
+		renderer.setYAxisMin(0);
+		renderer.setYAxisMax(210);
+
+		return renderer;
+	}
 
 }
