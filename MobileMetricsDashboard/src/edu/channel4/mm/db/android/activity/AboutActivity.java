5e0b724df160b936c39a4cfaa14ab3f256db4f6b
Brian Middaugh <bmiddaug@calpoly.edu>
2013-03-12 23:57:09 -0700

    Merging UI update branch

--- MobileMetricsDashboard/src/edu/channel4/mm/db/android/activity/AboutActivity.java
+++ MobileMetricsDashboard/src/edu/channel4/mm/db/android/activity/AboutActivity.java
@@ -1,23 +1,62 @@
 package edu.channel4.mm.db.android.activity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import edu.channel4.mm.db.android.R;
+import edu.channel4.mm.db.android.util.Log;
 
 public class AboutActivity extends Activity {
-   
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_about);
 
-		ListView list;
-		String list_array[] = getResources().getStringArray(
-				R.array.developer_names);
-		list = (ListView) findViewById(R.id.listViewDeveloperNames);
-		list.setAdapter(new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1, list_array));
+		// get listView from about activity
+		ListView list = (ListView) findViewById(R.id.listViewDeveloperNames);
+		// set the list view's adapter to be our custom array adapter
+		list.setAdapter(new AboutArrayAdapter(this, R.id.cellAbout,
+				getResources().getStringArray(R.array.developer_names)));
+	}
+
+	private class AboutArrayAdapter extends ArrayAdapter<String> {
+
+		private String[] developerNames;
+
+		public AboutArrayAdapter(Context context, int textViewResourceId,
+				String[] developerNames) {
+			super(context, textViewResourceId, developerNames);
+			this.developerNames = developerNames;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			if (developerNames == null) {
+				Log.e("appList List is null: will not draw cell");
+				return convertView;
+			}
+
+			// Grab the pertinent name
+			String name = developerNames[position];
+
+			// inflate the cell layout
+			LayoutInflater inflater = (LayoutInflater) getApplicationContext()
+					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			convertView = inflater.inflate(R.layout.cell_about, null);
+
+			//setting the cell's text
+			TextView developerTextView = (TextView) convertView
+					.findViewById(R.id.developerName);
+			developerTextView.setText(name);
+
+			return convertView;
+		}
 	}
 }
