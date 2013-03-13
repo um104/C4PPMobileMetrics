9bba9779e5353104b265cc53e1a7d6089a9f8af5
Girum Ibssa <ibssagirum@gmail.com>
2013-03-13 08:07:24 -0700

    Changed GraphViewerActivity spinners to use GraphType toString() instead of their all-caps enum names

--- MobileMetricsDashboard/src/edu/channel4/mm/db/android/model/graph/GraphType.java
+++ MobileMetricsDashboard/src/edu/channel4/mm/db/android/model/graph/GraphType.java
@@ -1,8 +1,19 @@
 package edu.channel4.mm.db.android.model.graph;
 
 public enum GraphType {
-	PIE,
-	BAR,
-	HISTOGRAM,
-	LINE;
+	PIE("Pie"),
+	BAR("Bar"),
+	HISTOGRAM("Histogram"),
+	LINE("Line");
+	
+	private String description;
+	
+	private GraphType(String description) {
+	   this.description = description;
+	}
+	
+	@Override
+	public String toString() {
+	   return description;
+	}
 }
