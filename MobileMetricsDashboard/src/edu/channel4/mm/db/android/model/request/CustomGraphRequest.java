c8630a4e46e66bffe8fc7c9be485919aebb17341
Mark Lerner <g.um104.w@gmail.com>
2013-03-13 03:32:57 -0700

    Added functionality to CustomGraphRequest so that it can now be used in the place of all other GraphRequests that eventually forward to Custom.

--- MobileMetricsDashboard/src/edu/channel4/mm/db/android/model/request/CustomGraphRequest.java
+++ MobileMetricsDashboard/src/edu/channel4/mm/db/android/model/request/CustomGraphRequest.java
@@ -15,6 +15,7 @@ import android.os.Parcel;
 import android.os.Parcelable;
 import edu.channel4.mm.db.android.R;
 import edu.channel4.mm.db.android.activity.EditGraphRequestActivity;
+import edu.channel4.mm.db.android.activity.GraphViewerActivity;
 import edu.channel4.mm.db.android.network.GraphRequestAsyncTask;
 import edu.channel4.mm.db.android.network.RestClientAccess;
 import edu.channel4.mm.db.android.network.SalesforceConn;
@@ -27,45 +28,49 @@ public class CustomGraphRequest implements GraphRequest, HasAttributeParameter,
    private final static String REQUEST_TYPE = "CUSTOM";
    private String eventName1;
    private String attribName1;
+   private boolean isPredefined;
+   private String graphName = "Custom Graph";
+   private int iconId;
 
    public CustomGraphRequest() {
+      this.graphName = "Custom Graph";
+      this.isPredefined = false;
+      this.iconId = R.drawable.ic_launcher;
+   }
+   
+   /**
+    * This constructor is to be used when you want to make a new pre-defined graph
+    * that is essentially a Custom Graph with its parameters set particularly.
+    * @param graphName The display name of the graph
+    * @param eventName1 The name of the event of the item -- usually an empty string
+    * @param attribName1 The name of the attribute in question.
+    */
+   public CustomGraphRequest(String graphName, String eventName1, String attribName1, int iconId) {
+      this.graphName = graphName;
+      this.eventName1 = eventName1;
+      this.attribName1 = attribName1;
+      this.iconId = iconId;
+      this.isPredefined = true;
+   }
+   
+   public String toString() {
+      return graphName;
    }
 
    public Intent constructGraphRequestIntent(Context context) {
-      Intent intent = new Intent(context, EditGraphRequestActivity.class);
+      Intent intent;
+      
+      // change what activity the Intent goes to depending on if it's predefined or not
+      intent = new Intent(context, (isPredefined)? EditGraphRequestActivity.class : GraphViewerActivity.class);
       intent.putExtra(Keys.GRAPH_REQUEST_EXTRA, this);
+      
 
       return intent;
    }
 
-   // @Override
-   // public String getRestRequestType() {
-   // return "CUSTOM";
-   // }
-   //
-   // @Override
-   // public String getUrlParameterString(Context context) {
-   // appLabel = context.getSharedPreferences(Keys.PREFS_NS, 0).getString(
-   // Keys.APP_LABEL, null);
-   //
-   // List<NameValuePair> params = new ArrayList<NameValuePair>();
-   //
-   // // Should be "custom" "SessionOverTime", "EventOverTime", etc.
-   // params.add(new BasicNameValuePair(Keys.REQUEST_TYPE,
-   // getRestRequestType()));
-   // params.add(new BasicNameValuePair(Keys.APP_LABEL_URL_PARAMETER_NAME,
-   // appLabel));
-   // params.add(new BasicNameValuePair(Keys.EVENT_NAME_1, eventName1));
-   // params.add(new BasicNameValuePair(Keys.ATTRIB_NAME_1, attribName1));
-   //
-   // String paramString = URLEncodedUtils.format(params, "utf-8");
-   //
-   // return paramString;
-   // }
-
    @Override
    public int getIconId() {
-      return R.drawable.ic_launcher;
+      return iconId;
    }
 
    @Override
