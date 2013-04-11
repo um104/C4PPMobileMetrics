90af731623ce1d376b5516d0b63f47ccedfa7da5
Mark Lerner <g.um104.w@gmail.com>
2013-04-11 10:48:30 -0700

    Removed commented out code.

--- MobileMetricsDashboard/src/edu/channel4/mm/db/android/model/description/AttributeDescription.java
+++ MobileMetricsDashboard/src/edu/channel4/mm/db/android/model/description/AttributeDescription.java
@@ -18,33 +18,4 @@ public class AttributeDescription {
 		return name;    
 	}
 
-	//
-	// @Override
-	// public int compareTo(AttributeDescription attribDesc) {
-	// int retval;
-	//
-	// // Both are session attribs
-	// if (attribEventName.length() == 0 &&
-	// attribDesc.getAttribEventName().length() == 0) {
-	// retval = attribName.compareTo(attribDesc.getAttribName());
-	// }
-	// // Both are event attribs
-	// else if (attribEventName.length() != 0 &&
-	// attribDesc.getAttribEventName().length() != 0) {
-	// if (attribEventName.equals(attribDesc.getAttribEventName())) { // same
-	// event
-	// retval = attribName.compareTo(attribDesc.getAttribName());
-	// }
-	// else {
-	// retval = attribEventName.compareTo(attribDesc.getAttribEventName());
-	// }
-	// }
-	// else { // either me or attribDesc is a session
-	// retval = (attribEventName.length() == 0) ? 1 : -1;
-	// }
-	//
-	// return retval;
-	// }
-
-
 }
